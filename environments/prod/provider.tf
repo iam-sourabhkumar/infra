7648e377dbb64b79b360f621c7018ae064603e2c
sourabhdev <sourabh792@gmail.com>
2026-08-10 01:05:20 +0530

    my first commit

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -1,11 +1,16 @@
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.80.0"
+      version = "5.0.1"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "sks01-dev"
+    storage_account_name = "stchorpreprod"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  } 
 }
 
 provider "azurerm" {
